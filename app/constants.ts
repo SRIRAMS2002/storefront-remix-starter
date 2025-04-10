744c1db93317367d7b7389c8b4e4aeb5efe2ee61
SRIRAMS2002 <sriramsrit.2002@gmail.com>
2025-04-10 10:10:26 +0530

    Basic Setup has been done

--- app/constants.ts
+++ app/constants.ts
@@ -7,15 +7,7 @@ export let API_URL =
     ? process.env.VENDURE_API_URL ?? DEMO_API_URL
     : DEMO_API_URL;
 
-/**
- * This function is used when running in Cloudflare Pages in order to set the API URL
- * based on an environment variable. Env vars work differently in CF Pages and are not available
- * on the `process` object (which does not exist). Instead, it needs to be accessed from the loader
- * context, and if defined we use it here to set the API_URL var which will be used by the
- * GraphQL calls.
- *
- * See https://developers.cloudflare.com/workers/platform/environment-variables/#environmental-variables-with-module-workers
- */
+
 export function setApiUrl(apiUrl: string) {
   API_URL = apiUrl;
 }
